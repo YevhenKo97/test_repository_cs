5f8883f966096089ba3cc0a47dc1aa40a8803411
YevhenKo97 <145015139+YevhenKo97@users.noreply.github.com>
2025-07-03 15:05:41 +0300

    Update postCreateCommands.sh pwsh env var usage

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/.local/share/powershell/Modules/QOpsModule && cp -r /workspaces/test_repository_cs/QOpsModule ~/.local/share/powershell/Modules/
 pwsh -c "QOps-Version -NoAutoUpdate"
-pwsh -c "QOps-SetLicense ${{ secrets.QOPS_LICENSE_KEY }}"
+pwsh -c "QOps-SetLicense $Env:QOPS_LICENSE_KEY"
 pwsh -c "QOps-Activate"
