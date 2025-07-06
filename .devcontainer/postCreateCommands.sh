c99fc40f82655333109a178741e8ce47a46755c2
YevhenKo97 <145015139+YevhenKo97@users.noreply.github.com>
2025-07-06 16:56:52 +0300

    Update postCreateCommands.sh activation fix

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,4 +1,3 @@
 mkdir -p ~/.local/share/powershell/Modules/QOpsModule && cp -r /workspaces/test_repository_cs/QOpsModule ~/.local/share/powershell/Modules/
 pwsh -c "QOps-Version -NoAutoUpdate"
-pwsh -c "QOps-SetLicense $Env:QOPS_LICENSE_KEY"
-pwsh -c "QOps-Activate"
+pwsh -c "QOps-Activate -Value $Env:QOPS_LICENSE_KEY -Force"
